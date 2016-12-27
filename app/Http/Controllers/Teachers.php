1e334fc7af29cbf2a5a36c64ed14c1e8e8488d14
Aslanbek Dudaev <Nemesis@Nemesis-2.local>
2016-12-27 09:59:05 +0300

    -

--- app/Http/Controllers/Teachers.php
+++ app/Http/Controllers/Teachers.php
@@ -52,7 +52,7 @@ class teachers extends Controller {
 
 
 	public function setMark(Request $request,App\Lesson $lesson,App\Group $group,App\Student $student) {
-		if($this->user->id!=71&&$this->user->id!=35)return $this->err('Система временно закрыта');
+		// if($this->user->id!=71&&$this->user->id!=35)return $this->err('Система временно закрыта');
 		// \Illuminate\Support\Facades\DB::enableQueryLog();
 		if(Validator::make($request->all(),array('type'=>array('required','regex:'.Helper::types(true)),'mark'=>'required|regex:/^[0-9]+$/'))->fails())return $this->logout('m_regex');
 		$d = explode('-', date('Y-n'));
